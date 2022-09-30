c27912aab87e31e26b68c27cd1380ab5fb974c86
MarkkuVaara72 <markku.vaara@solenovo.fi>
2022-09-30 22:10:22 +0300

    Preliminary cards

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ const App = (props) => {
   return (
 
     <div className="main">
-      <h1>Integrify React Assignment</h1>
-      {content.map(person => 
-        <div>
-          <p>Id: {person.id}</p>
-          <p>Name: {person.name}</p>
-          <p>Username: {person.username}</p>
-          <p>E-mail: {person.email}</p>
-          <p>Address: {JSON.stringify(person.address)}</p>
-          <p>Phone: {person.phone}</p>
-          <p>Website: {person.website}</p>
-          <p>Company: {JSON.stringify(person.company)}</p>
-        </div>
-      )}
+      <h1>User information</h1>
+      <div className="cards">
+        {content.map(person => 
+          <div className="cardcontent">
+            <p>Id: {person.id}</p>
+            <p>Name: {person.name}</p>
+            <p>Username: {person.username}</p>
+            <p>E-mail: {person.email}</p>
+            <p>Address: {JSON.stringify(person.address)}</p>
+            <p>Phone: {person.phone}</p>
+            <p>Website: {person.website}</p>
+            <p>Company: {JSON.stringify(person.company)}</p>
+          </div>
+        )}
+      </div>
     </div>
 
   );
