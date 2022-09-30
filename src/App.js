fc7c781f34071926724fe74d336af740d081008e
MarkkuVaara72 <markku.vaara@solenovo.fi>
2022-09-30 21:51:19 +0300

    Added hard content for development

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
-function App() {
+const App = (props) => {
+
+  const content = props.content;
+
   return (
+
     <div className="main">
       <h1>Integrify React Assignment</h1>
-      <p>Test</p>
+      {content.map(person => 
+        <div>
+          <p>Id: {person.id}</p>
+          <p>Name: {person.name}</p>
+          <p>Username: {person.username}</p>
+          <p>E-mail: {person.email}</p>
+          <p>Address: {JSON.stringify(person.address)}</p>
+          <p>Phone: {person.phone}</p>
+          <p>Website: {person.website}</p>
+          <p>Company: {JSON.stringify(person.company)}</p>
+        </div>
+      )}
     </div>
+
   );
+
 }
 
 export default App;
