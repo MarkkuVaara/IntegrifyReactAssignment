8e62cb92decc25880fc967a853663a3e1ec9bd1a
MarkkuVaara72 <markku.vaara@solenovo.fi>
2022-09-30 22:40:13 +0300

    Adding state and card styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
+import React, { useState, useEffect } from "react";
 
 const App = (props) => {
 
-  const content = props.content;
+  const [content, setContent] = useState(props.content);
 
   return (
 
     <div className="main">
       <h1>User information</h1>
+
       <div className="cards">
         {content.map(person => 
           <div className="cardcontent">
-            <p>Id: {person.id}</p>
             <p>Name: {person.name}</p>
             <p>Username: {person.username}</p>
             <p>E-mail: {person.email}</p>
-            <p>Address: {JSON.stringify(person.address)}</p>
+            <p>Address:</p>
+            <div className="address">
+              <p>Street: {person.address.street}</p>
+              <p>Suite: {person.address.suite}</p>
+              <p>City: {person.address.city}</p>
+              <p>Zipcode: {person.address.zipcode}</p>
+              <div className="geo">
+                <p>Geo(latitude): {person.address.geo.lat}</p>
+                <p>Geo(longitude): {person.address.geo.lng}</p>
+              </div>
+            </div>
             <p>Phone: {person.phone}</p>
             <p>Website: {person.website}</p>
-            <p>Company: {JSON.stringify(person.company)}</p>
+            <p>Company:</p>
+            <div className="company">
+              <p>Name: {person.company.name}</p>
+              <p>Catchphrase: {person.company.catchPhrase}</p>
+              <p>Bs: {person.company.bs}</p>
+            </div>
           </div>
         )}
       </div>
+
     </div>
 
   );
